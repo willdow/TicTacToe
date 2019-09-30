5bf553cfe2b77473a75f4e343cc8f500574cc852
Willyan LIN <willyanlin@MAC045.local>
2019-09-30 14:58:06 +0200

    finish tuto react and add css

--- my-app/src/components/Square/Square.jsx
+++ my-app/src/components/Square/Square.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import css from "./square.css";
+
 
 class Square extends React.Component {
     render() {
         return (
-            <button className="square">
-                {/* TODO */}
+            <button 
+                className={`square `+ this.props.value} 
+                onClick={()=>this.props.onClick()}>
+                    {this.props.value}
             </button>
         );
     }
